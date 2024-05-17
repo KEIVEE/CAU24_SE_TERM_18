bc912db453f9f60d496065d90e1cf0787eb81b23
KEIVEE <kewanee01@gmail.com>
2024-05-17 21:38:02 +0900

    새 창 띄우기, 원래 창 없애기 새 창은 탭 형식.

--- GuiTest.java
+++ GuiTest.java
@@ -1,6 +1,51 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class Myframe extends JFrame{
+    public Myframe(String title){
+        this.setTitle(title);
+        this.setSize(800, 600);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public JTabbedPane createTab(){
+        JTabbedPane pane = new JTabbedPane();
+
+        pane.addTab("New", new JLabel("Tab menu 1"));
+        pane.addTab("Browse", new JLabel("Tab menu 2"));
+        pane.addTab("", new JLabel("Tab menu 3"));
+
+        return pane;
+    }
+}
+
 public class GuiTest {
 
     public static void main(String[] args){
-        System.out.println("Gui test starting");
+        Myframe login = new Myframe("Log in");
+
+        JButton loginButton = new JButton("Log In");
+        loginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+
+                Myframe frame = new Myframe("Issue Handling");
+                login.dispose();
+
+                JTabbedPane pane = frame.createTab();
+                frame.add(pane);
+
+            }
+        });
+        login.add(loginButton);
+
+        login.revalidate();
+        login.repaint();
+        //JTabbedPane pane = frame.createTab();
+        //frame.add(pane);
+
+        //JButton
     }
 }
