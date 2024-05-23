bca89a70b85d9cc6573fc5c3b2fbbe4d78ccef2a
chch6879 <162443374+chch6879@users.noreply.github.com>
2024-05-23 14:13:17 +0900

    Add files via upload

---
+++ IssueService.java
@@ -0,0 +1,12 @@
+package com.IssueTracking.service;
+
+public class IssueService {
+
+
+}
+
+/*
+* 이슈 기능
+* 1. changeFixer \\ Assignee \\ Reporter
+* 2.
+*/
---
+++ UserService.java
@@ -0,0 +1,20 @@
+package com.IssueTracking.service;
+
+import com.IssueTracking.model.Comment;
+import com.IssueTracking.model.Status;
+import com.IssueTracking.model.User;
+
+public class UserService {
+
+
+}
+/*
+* 유저 기능의 종류
+* 1. findByName \\ findAll --> 무엇을? issue? comment? user?
+* 2. createIssue
+* 3. deleteIssue
+* 4. addComment
+* 5. assignIssue
+* 6. changeIssueStatus --> 누가 이슈 상태를 바꿀 것인가? user? issue?
+* 7. 통계 분석을 위한 메서드들 --> ex) findByDate
+* */
